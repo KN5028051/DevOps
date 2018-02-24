b0aeba22fdffff7f067731e184ff062c172624f8
Kunal namdev <namdev.kunal0@gmail.com>
2018-02-25 00:35:10 +0530

    initial commit

---
+++ swarmExample/src/main/java/com/swarm/example/swarmExample/App.java
@@ -0,0 +1,13 @@
+package com.swarm.example.swarmExample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
